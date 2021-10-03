c617ced50e9a1d1bc2383543aeb90b3739a99276
airRnot1106 <yngw.rintaro@gmail.com>
2021-10-03 23:41:59 +0900

    Testの仕様を変更
    Testはtsではなくjsにするよう変更。これはrequireのテストも兼ねている

--- test/test.js
+++ test/test.js
@@ -1,21 +1,26 @@
-"use strict";
-exports.__esModule = true;
-var index_1 = require("../dist/index");
+const Cology = require('../dist/index');
+
 //Normal Test
-index_1["default"].log('debug', 'debug test');
-index_1["default"].log('info', 'info test');
-index_1["default"].log('success', 'success test');
-index_1["default"].log('warn', 'warn test');
-index_1["default"].log('error', 'error test');
-index_1["default"].log('fatal', 'fatal test');
+Cology.log('debug', 'debug test');
+Cology.log('info', 'info test');
+Cology.log('success', 'success test');
+Cology.log('warn', 'warn test');
+Cology.log('error', 'error test');
+Cology.log('fatal', 'fatal test');
+
 //Border Test
-index_1["default"].setBorder('--------');
-index_1["default"].log('info', 'Test:\nGlobal Border Test In Progress');
-index_1["default"].push();
-index_1["default"].setBorder('<><><><>');
-index_1["default"].log('info', 'Test:\nTemporary Border Test In Progress');
-index_1["default"].pop();
-index_1["default"].log('info', 'Test:\nGlobal Border Test In Progress Again');
-index_1["default"].setBorder('');
-index_1["default"].log('success', 'Test Completed');
+Cology.setBorder('--------');
+Cology.log('info', 'Test:\nGlobal Border Test In Progress');
+Cology.push();
+Cology.setBorder('<><><><>');
+Cology.log('info', 'Test:\nTemporary Border Test In Progress');
+Cology.pop();
+Cology.log('info', 'Test:\nGlobal Border Test In Progress Again');
+
+//Date Test
+Cology.date();
+
+Cology.setBorder('');
+Cology.log('success', 'Test Completed');
+
 process.exit(0);
